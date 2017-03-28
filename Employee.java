d7a64d6552c5c025f27a18232fa5f9426a04a628
Swapna-SpringPeople <swapnaspringjavascript@gmail.com>
2017-03-28 17:40:13 +0530

    Model

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package com.sakha.model;
+
+import com.sakha.spring.aspect.Loggable;
+
+public class Employee {
+
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	@Loggable
+	public void setName(String nm) {
+		this.name=nm;
+	}
+	
+	public void throwException(){
+		throw new RuntimeException("Dummy Exception");
+	}	
+}
