f345f9ae2cca8c09cfedba3d6e5dcd483b3ac641
Swapna-SpringPeople <swapnaspringjavascript@gmail.com>
2017-03-28 17:40:38 +0530

    Service

---
+++ EmployeeService.java
@@ -0,0 +1,16 @@
+package com.sakha.service;
+
+import com.sakha.model.Employee;
+
+public class EmployeeService {
+
+	private Employee employee;
+	
+	public Employee getEmployee(){
+		return this.employee;
+	}
+	
+	public void setEmployee(Employee e){
+		this.employee=e;
+	}
+}
